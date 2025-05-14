641c7bdceca7fdf4f3a983014707d84be19d0161
YVNNVLEX <nguessankouakouyannalex@gmail.com>
2025-05-14 13:35:03 +0000

    reform backend

--- Backend/e_santebackend/e_santebackend/settings.py
+++ Backend/e_santebackend/e_santebackend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'qrcode',
+    'corsheaders',
     'reportlab',
     'djoser'
 ]
@@ -74,11 +75,16 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'e_santebackend.urls'
